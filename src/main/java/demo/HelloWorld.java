2b3f0b48dfcd2ff087bcfa4d0f16df4e7fc6784e
Kenji <kenji@icloud.com>
2025-06-16 22:26:15 +0100

    在分支上新增内容，将新内容写入到文件fos.dat中

--- src/main/java/demo/HelloWorld.java
+++ src/main/java/demo/HelloWorld.java
@@ -11,6 +11,8 @@ public class HelloWorld {
         FileOutputStream fos = new FileOutputStream("fos.dat");
         String line = "犬は机の上にいます";
         fos.write(line.getBytes(StandardCharsets.UTF_8));
+        line = "分支上写入的内容";
+        fos.write(line.getBytes(StandardCharsets.UTF_8));
         System.out.println("写出完毕");
         fos.close();
     }
